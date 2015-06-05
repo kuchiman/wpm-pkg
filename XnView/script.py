3b14a42ea817d4148f8ee28cd237e679c8f55d72
kuchiman <pro@ssau.ru>
2015-06-05 13:52:11 +0400

    правки

--- XnView/script.py
+++ XnView/script.py
@@ -1,57 +1,36 @@
-import os
+# -*- coding: utf-8 -*-
 import sys
-import platform
-import subprocess
+
+sys.path.append(sys.argv[1])
+from scriptlib import *
 
 """http://www.itninja.com/software/gougelet-pierre-e/xnview/1-8655"""
 
-if platform.machine() == 'AMD64':
-    PROGRAMDIR = 'C:\Program Files (x86)\XnView'
-else:
-    PROGRAMDIR = 'C:\Program Files\XnView'
+"""Файлы пакета"""
+FILES = (
+    'XnView-win-full.exe',
+    'associate.reg'
+)
 
 """Имена исполняемых файлов"""
-INSTALLER = 'XnView-win-full.exe'
-UNINSTALLER = 'unins000.exe'
-ASSOCIATE = 'associate.reg'
+INSTALLER = os.path.join('', DIR, FILES[0])
+UNINSTALLER = os.path.join('', SYSDIR, 'XnView', 'unins000.exe')
 
-"""Текущая директория"""
-DIR = os.path.dirname(sys.argv[0])
-
-"""Полный путь к исполняемым файлам"""
-BNINSTALLER = os.path.join('', DIR, INSTALLER)
-BNUNINSTALLER = os.path.join('', PROGRAMDIR, UNINSTALLER)
-BNASSOCIATE = os.path.join('', DIR, ASSOCIATE)
-
-
-def check_files():
-    if not os.path.isfile(BNINSTALLER) or not os.path.isfile(BNASSOCIATE):
-        print("Отсутствует инсталлятор!!")
-        sys.exit()
-
-
-def taskkill():
-    subprocess.call(['taskkill.exe', '/F', '/T',
-        '/IM', INSTALLER, '/IM', UNINSTALLER],
-        shell=False, stdout=subprocess.PIPE, stderr=sys.stdout)
-
-
-def remove():
-    if os.path.isfile(BNUNINSTALLER):
-        subprocess.call([BNUNINSTALLER, '/verysilent'],
-            shell=False, stdout=subprocess.PIPE, stderr=sys.stdout)
+"""Файл реестра"""
+ASSOCIATE = os.path.join('', DIR, FILES[1])
 
 
 def install():
-    subprocess.call(['regedit', '/S', BNASSOCIATE],
-        shell=False, stdout=subprocess.PIPE, stderr=sys.stdout)
-    subprocess.call([BNINSTALLER, '/verysilent'],
-        shell=False, stdout=subprocess.PIPE, stderr=sys.stdout)
+    run_exe(INSTALLER, '/verysilent')
+    run_exe('regedit', '/S', ASSOCIATE)
 
-check_files()
-taskkill()
 
-if sys.argv[1] == 'install':
+def remove():
+    run_exe(UNINSTALLER, '/verysilent')
+
+check_files(FILES)
+
+if ACTION == 'install':
     install()
-elif sys.argv[1] == 'remove':
+elif ACTION == 'remove':
     remove()
---
+++ chrome/script.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import sys
+
+sys.path.append(sys.argv[1])
+from scriptlib import *
+
+"""Файлы пакета"""
+FILES = (
+    'Firefox Setup 37.0b7.exe',
+    'all.js'
+)
+
+"""Имена исполняемых файлов"""
+INSTALLER = os.path.join('', DIR, FILES[0])
+UNINSTALLER = os.path.join('', SYSDIR, 'Mozilla Firefox',
+    'uninstall', 'helper.exe')
+
+"""Конфиг"""
+CONFDIR = os.path.join('', SYSDIR, 'Mozilla Firefox', 'defaults', 'pref')
+
+
+def install():
+    run_exe(INSTALLER, '/s')
+    copy(FILES[1], CONFDIR)
+
+
+def remove():
+    run_exe(UNINSTALLER, '/s')
+
+check_files(FILES)
+
+if ACTION == 'install':
+    install()
+elif ACTION == 'remove':
+    remove()
